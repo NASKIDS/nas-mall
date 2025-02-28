baea5515f692c0ed53ea70ad3066279e656104c8
许德轩 <dexuan.xu@mihoyo.com>
2025-03-01 02:36:17 +0800

    feat: 初步实现核心代码

--- app/auth/biz/service/ban_user.go
+++ app/auth/biz/service/ban_user.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 
+	"github.com/cloudwego/kitex/pkg/klog"
+
 	"github.com/naskids/nas-mall/app/auth/biz/model"
 	auth "github.com/naskids/nas-mall/rpc_gen/kitex_gen/auth"
 )
@@ -20,7 +22,7 @@ func (s *BanUserService) Run(req *auth.BanUserReq) (resp *auth.BanUserResp, err 
 	ids := req.UserIds
 	for _, id := range ids {
 		// 加入用户黑名单
-
+		klog.Println(id)
 		// 删除token白名单
 
 		// 删除用户 auth 信息
